8b78bfecb5a424a9ffe69121226a2124d007673c
mzp <mzpppp@gmail.com>
2015-07-18 17:04:04 +0900

    :shell: Add BrewSparkling::Handler to handle command

--- lib/brew_sparkling/cli.rb
+++ lib/brew_sparkling/cli.rb
@@ -9,6 +9,19 @@ module BrewSparkling
     end
 
     def call
+      unless Handler.exist? command_name
+        STDERR.puts "unknown command: #{command_name}"
+        # TODO: show help
+        return
+      end
+
+      Handler.invoke command_name, args
+    end
+
+    private
+
+    def command_name
+      @command.to_sym
     end
   end
 end
---
+++ lib/brew_sparkling/handler.rb
@@ -0,0 +1,25 @@
+module BrewSparkling
+  module Handler
+    class << self
+      def register(name, klass)
+        @@command ||= {}
+        @@command[name] = klass
+      end
+
+      def invoke(name, args)
+        command = @@command[name].new
+        command.send name, *args
+      end
+
+      def exist?(name)
+        @@command.key? name
+      end
+
+      def commands
+        @@command.keys.sort
+      end
+    end
+  end
+end
+
+require 'brew_sparkling/handler/base'
