67a20275d15fe520234f8493f3f362604870295f
mzp <mzpppp@gmail.com>
2015-07-21 08:22:38 +0900

    :shell: add change_id command

--- lib/brew_sparkling/recipe/recipe.rb
+++ lib/brew_sparkling/recipe/recipe.rb
@@ -22,6 +22,14 @@ module BrewSparkling
         Location.archive_path.join(name, version)
       end
 
+      def info_plists
+        xs = []
+        traversal build_path do |entry|
+          xs << entry if entry.basename.fnmatch? 'Info.plist'
+        end
+        xs
+      end
+
       def app_path
         Location.archive_path.join(name, "#{version}.xcarchive", 'Products', 'Applications', "#{app_name}.app")
       end
@@ -36,6 +44,23 @@ module BrewSparkling
 
       def build
         instance_eval(&@build_code)
+      end
+
+      private
+
+      def traversal(path, &f)
+        directories = [ path ]
+
+        until directories.empty? do
+          d = directories.shift
+          d.each_child do |entry|
+            if entry.directory?
+              directories << entry
+            else
+              f.call entry
+            end
+          end
+        end
       end
     end
   end
