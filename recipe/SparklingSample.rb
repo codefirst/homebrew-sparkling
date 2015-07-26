9cf0cb770d0c4bbe78108e7a0f975b64dff6c61c
mzp <mzpppp@gmail.com>
2015-07-26 21:36:13 +0900

    :cocktail: SparklingSample: fix typo

--- recipe/SparklingSample.rb
+++ recipe/SparklingSample.rb
@@ -1,12 +1,12 @@
 recipe do
   name 'SparklingSample'
-  url 'https://github.com/codefirst/SparklingSample/archive/1.0.tar.gz'
-  version '1.0'
-  bundle_identifier 'jp.codefirst.SparklingSample'
+  url 'https://github.com/codefirst/SparklingSample/archive/1.0.1.tar.gz'
+  version '1.0.1'
+  bundle_identifier 'org.codefirst.SparklingSample'
 
   build do
     system <<-END
-            cd #{build_path}/SparklingSample-1.0/
+            cd #{build_path}/SparklingSample-1.0.1/
             xcodebuild -scheme SparklingSample -archivePath #{archive_path} archive #{xcpretty}
             END
   end
