6d7447e6f3c4d1669f58e2b8c73dd9754f4be72a
mzp <mzpppp@gmail.com>
2015-09-17 09:47:03 +0900

    :cocktail: update SparklingSample recipe

--- recipe/SparklingSample.rb
+++ recipe/SparklingSample.rb
@@ -1,7 +1,7 @@
 class SparklingSample < BrewSparkling::Recipe::Builder
-  github 'codefirst/SparklingSample', branch: '1.0.1'
+  github 'codefirst/SparklingSample', branch: '1.0.3'
   description 'sample for Sparkling'
-  version '1.0.1'
+  version '1.0.3'
   bundle_identifier 'org.codefirst.SparklingSample'
 
   def build
